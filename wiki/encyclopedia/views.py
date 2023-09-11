d0c3543e14a2cda36016577d4224cc3636f2e7cc
Mihai Paun <email.mihu@gmail.com>
2023-09-12 00:19:08 +0200

    Wrote the convert_md_to_html function.

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
+from markdown2 import Markdown
 
 from . import util
 
+def convert_md_to_html(title):
+    """
+    Converts a MD file to HTML
+
+    :param title: The title of the MD file we want to convert.
+    :type title: String
+    """
+    content = util.get_entry(title)
+    markdowner = Markdown()
+    
+    # check if the file exists (content != None)
+    if content == None:
+        return None
+    else:    
+        return markdowner.convert(content)
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
