b17ae244a59a9351f1855645dda441b0db790533
Mihai Paun <email.mihu@gmail.com>
2023-09-12 00:19:23 +0200

    Wrote a test case for the convert_md_to_html func.

--- wiki/encyclopedia/tests.py
+++ wiki/encyclopedia/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from .views import convert_md_to_html
+from .util import get_entry
 
 # Create your tests here.
+class ConvertMdToHtml(TestCase):
+    def test_conv(self):
+        html_content = convert_md_to_html("CSS")
+        print(html_content)
+        self.assertIsNotNone(html_content)
